9a836688035fbfbb347119038f94a8d6a1104c2b
sforster <syslive-creative-co@outlook.com>
2023-11-19 22:06:30 +0100

    dim 19 nov

--- test.c
+++ test.c
@@ -6,7 +6,7 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 08:50:30 by sforster          #+#    #+#             */
-/*   Updated: 2023/11/16 20:18:32 by marvin           ###   ########.fr       */
+/*   Updated: 2023/11/19 21:11:34 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,9 @@
 int main(void)
 {
 	printf("I was in Paris this weekend for 4 days\n");
-	printf("I was in %s this weekend for %d days\n", "Paris");
+	printf("I was in %s this weekend for %d days\n", "Paris", 12);
+
+//	printf("testpointer %d", )
+	printf("test petit x %x \n", 15);
+	printf("test grand X %X \n", 125642);
 }
