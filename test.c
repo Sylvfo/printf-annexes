3d276c5086d32d8e0c50dcba88d4c22850165b80
Sylvie Forster <sforster@c1r7s3.42lausanne.ch>
2023-11-20 11:45:52 +0100

    lundi20novmidi

--- test.c
+++ test.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   test.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: sforster <sforster@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 08:50:30 by sforster          #+#    #+#             */
-/*   Updated: 2023/11/19 21:11:34 by marvin           ###   ########.fr       */
+/*   Updated: 2023/11/20 11:45:06 by sforster         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,51 @@ int main(void)
 	printf("I was in %s this weekend for %d days\n", "Paris", 12);
 
 //	printf("testpointer %d", )
-	printf("test petit x %x \n", 15);
+	printf("test petit 1 x %x \n", 1);
+	printf("test petit 2 x %x \n", 2);
+	printf("test petit 3 x %x \n", 3);
+	printf("test petit 4 x %x \n", 4);
+	printf("test petit 5 x %x \n", 5);
+	printf("test petit 6 x %x \n", 6);
+	printf("test petit 9 x %x \n", 9);
+	printf("test petit 10 x %x \n", 10);
+	printf("test petit 11 x %x \n", 11);
+	printf("test petit 12 x %x \n", 12);
+	printf("test petit 13 x %x \n", 13);
+	printf("test petit 14 x %x \n", 14);
+	printf("test petit 15 x %x \n", 15);
+	printf("test petit 16 x %x \n", 16);
+	printf("test petit 17 x %x \n", 17);
+	printf("test petit 18 x %x \n", 18);	
+	printf("test petit 19 x %x \n", 19);
+	printf("test petit 20 x %x \n", 20);
+	printf("test petit 21 x %x \n", 21);
+	printf("test petit 22 x %x \n", 22);
+	printf("test petit 23 x %x \n", 23);
+	printf("test petit 24 x %x \n", 24);
+	printf("test petit 25 x %x \n", 25);
+	printf("test petit 26 x %x \n", 26);
+	printf("test petit 27 x %x \n", 27);	
+	printf("test petit 28 x %x \n", 28);
+	printf("test petit 29 x %x \n", 29);
+	printf("test petit 30 x %x \n", 30);
+	printf("test petit 31 x %x \n", 31);
+	printf("test petit 32 x %x \n", 32);
+	printf("test petit 33 x %x \n", 33);	
+	printf("test petit 34 x %x \n", 34);
+	printf("test petit 100 x %x \n", 100);
+	printf("test petit 2023 x %x \n", 2023);
+	printf("test petit 200 x %x \n", 200);
+	printf("test petit 1000 x %x \n", 2000);
+	printf("test petit 300 x %x \n", 300);	
+	printf("test petit 301 x %x \n", 301);
+	printf("test petit 101 x %x \n", 101);
+	printf("test petit 102 x %x \n", 102);
+	printf("test petit 150 x %x \n", 150);
+	printf("laaatest petit 160 x %x \n", 160);	
+	printf("test petit 161 x %x \n", 161);
+	printf("test petit 320 x %x \n", 320);
+	printf("test petit 319 x %x \n", 319);
+	
 	printf("test grand X %X \n", 125642);
 }
